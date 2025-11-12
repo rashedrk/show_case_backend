72184fc96fd4d9043503c326472685cf7b103729
rashedrk <rashedrk001@gmail.com>
2025-11-12 12:40:29 +0600

    user services- createUser, getUserByEmail implemented

---
+++ src/app/modules/User/user.service.ts
@@ -0,0 +1,19 @@
+import { TUser, TUserCreationAttributes } from './user.interface';
+import User from './user.model';
+
+const createUser = async (
+  userData: TUserCreationAttributes,
+): Promise<TUser> => {
+  const user = await User.create(userData);
+  return user.toJSON();
+};
+
+const getUserByEmail = async (email: string): Promise<TUser | null> => {
+  const user = await User.findOne({ where: { email } });
+  return user ? user.toJSON() : null;
+};
+
+export const userService = {
+  createUser,
+  getUserByEmail,
+};
