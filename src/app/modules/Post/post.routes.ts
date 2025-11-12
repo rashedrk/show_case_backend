6edea95755ef06ce03312bed85382b424d1e589f
rashedrk <rashedrk001@gmail.com>
2025-11-12 23:54:15 +0600

    post api endpoint fix - userId confilict

--- src/app/modules/Post/post.routes.ts
+++ src/app/modules/Post/post.routes.ts
@@ -4,10 +4,10 @@ import { postControllers } from './post.controllers';
 
 const router = Router();
 
-router.get('/:userId', postControllers.getAllPostsByUserId);
 router.post('/', auth('user'), postControllers.createPost);
 router.get('/:id', postControllers.getPostById);
 router.put('/:id', auth('user'), postControllers.updatePost);
 router.delete('/:id', auth('user'), postControllers.deletePost);
+router.get('/user/:userId', postControllers.getAllPostsByUserId);
 
 export const postRoutes = router;
