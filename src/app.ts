525eeaa890111837b0d234bc32ac1e6e5591d57c
rashedrk <rashedrk001@gmail.com>
2025-11-12 12:55:53 +0600

    route added to the app and fix the utlits path

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -19,5 +20,7 @@ app.get('/', (req: Request, res: Response) => {
     message: 'Welcome to Show Case Backend API!',
   });
 });
+
+app.use('/api/v1', router);
 
 export default app;
--- src/app/modules/User/user.controllers.ts
+++ src/app/modules/User/user.controllers.ts
@@ -1,5 +1,5 @@
-import catchAsync from '../../../utils/catchAsync';
-import sendResponse from '../../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 import { userServices } from './user.service';
 import httpStatus from 'http-status';
 
